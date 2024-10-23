9098749522e4761f96868f0f8d7e57b940017313
AlekseyVoronkov <voronkovlesha42@mail.ru>
2024-10-24 00:29:30 +0700

    unfinished project commit

---
+++ app/src/main/java/com/example/gson/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.gson
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import timber.log.Timber
+import timber.log.Timber.Forest.plant
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        plant(Timber.DebugTree())
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ff49fcd4d4a08aa6aafb6ea3de826464&tags=cat&format=json&nojsoncallback=1")
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw Exception("error")
+
+            val wrapper = gson
+        }
+    }
+
+}
+
+data class Photo(
+    val id: String,
+    val owner: String,
+    val secret: String,
+    val server: String,
+    val farm: Int,
+    val title: String,
+    val ispublic: Int,
+    val isfriend: Int,
+    val isfamily: Int
+)
+
+data class PhotoPage(
+    val page: Int,
+    val pages: Int,
+    val perpage: Int,
+    val total: Int,
+    val photo: List<Photo>
+)
+
+data class Wrapper(
+    val photos: PhotoPage
+)
+
